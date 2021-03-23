4148b95f89f57d32b40396b693815ed2f8af2341
KyklyMachine <71447642+KyklyMachine@users.noreply.github.com>
2021-03-23 13:15:05 +0300

    getN now in tests!

--- tests.c
+++ tests.c
@@ -27,19 +27,22 @@ void* floatVectorTests()
     printf("Expected result: [3.70, 5.70, 7.70]\n");
     printf("Result of function SumN: ");
     printVectorFloat(v_summ);
+    printf("Third coordinate of the resulting vector: %.2f\n", *(float*)GetN(2, v_summ));
     
     printf("\nSecond test. Subtraction of vectors.\n");
     struct VectorN* v_subt = MinusN(v1, v2);
     printf("Expected result: [-0.70, -0.70, -0.70]\n");
     printf("Result of function minusN: ");
     printVectorFloat(v_subt);
+    printf("First coordinate of the resulting vector: %.2f\n", *(float*)GetN(0, v_subt));
     
     printf("\nThird test. Scalar multiplication of vectors.\n");
     struct VectorN* v_mult = ScalarMultN(v1, v2);
     printf("Expected result: [3.30, 8.00, 14.70]\n");
     printf("Result of function scalarMultN: ");
     printVectorFloat(v_mult);
-    printf("============================================================\n");
+    printf("Second coordinate of the resulting vector: %.2f", *(float*)GetN(1, v_mult));
+    printf("\n============================================================\n");
     return 0;
 };
 
@@ -53,23 +56,30 @@ void* complexVectorTests()
     printf("Two vectors with coordinates:\n");
     printVectorComplex(v1);
     printVectorComplex(v2);
+    
     printf("\nFirst test. Summ of vectors.\n");
     struct VectorN* v_summ = SumN(v1, v2);
     printf("Expected result: [(7.50; 8.00i), (7.00; 7.00i), (8.00; 7.50i)]\n");
     printf("Result of function SumN: ");
     printVectorComplex(v_summ);
+    printf("Third coordinate of the resulting vector: ");
+    printComplexCoord(GetN(2, v_summ));
     
-    printf("\nSecond test. Subtraction of vectors.\n");
+    printf("\n\nSecond test. Subtraction of vectors.\n");
     struct VectorN* v_subt = MinusN(v1, v2);
     printf("Expected result: [(-4.50; -3.60i), (-1.00; 1.00i), (3.60; 4.50i)]\n");
     printf("Result of function minusN: ");
     printVectorComplex(v_subt);
+    printf("Second coordinate of the resulting vector: ");
+    printComplexCoord(GetN(1, v_subt));
     
-    printf("\nThird test. Scalar multiplication of vectors.\n");
+    printf("\n\nThird test. Scalar multiplication of vectors.\n");
     struct VectorN* v_mult = ScalarMultN(v1, v2);
     printf("Expected result: [(-3.76; 21.90i), (0.00; 25.00i), (3.76; 21.90i)]\n");
     printf("Result of function scalarMultN: ");
     printVectorComplex(v_mult);
-    printf("============================================================\n");
+    printf("First coordinate of the resulting vector: ");
+    printComplexCoord(GetN(0, v_mult));
+    printf("\n============================================================\n");
     return 0;
 };
