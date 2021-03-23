e277135d7780d3059c8e1830074dd8750f5a1efe
KyklyMachine <71447642+KyklyMachine@users.noreply.github.com>
2021-03-23 13:20:57 +0300

    Added description

--- vector.h
+++ vector.h
@@ -6,14 +6,15 @@
 
 struct VectorN
 {
-    size_t dimension;
-    size_t elementSize;
+    size_t dimension;                   //Vector dimension
+    size_t elementSize;                 //Size of 1 element
     void* zero;                         //∃(0): a + 0 = a
     void* one;                          //∃(1): a * 1 = a
-    void* coordinates;
+    void* coordinates;                  //Array of coordinates
     void* (*minus)(void*);              //∃(a*): a + a* = 0
     void* (*sum)(void* x, void* y);     //Determination of the sum of 2 elements
     void* (*mult)(void* x, void* y);    //Determination of the multiplication of 2 elements
+                                        //There should be an inverted item here 
 };
 
 //I.Vector creating
@@ -32,5 +33,7 @@ struct VectorN* MinusN(struct VectorN* v1, struct VectorN* v2);
 
 //Output errors
 void* Error(char* msg);
+
+#endif /* vector_c_h */
 
 #endif /* vector_c_h */
